c1e109ed6e7b502bddaff64fbd6753aa829b095c
Watson Ladd <watson@cloudflare.com>
2019-03-21 14:53:48 -0700

    Finally works!

--- src/nts_ke/server.rs
+++ src/nts_ke/server.rs
@@ -1,6 +1,5 @@
 use std::net::ToSocketAddrs;
-use std::sync::Arc;
-
+use std::sync::{Arc, RwLock};
 use std::vec::Vec;
 
 extern crate rustls;
@@ -19,6 +18,7 @@ use tokio::prelude::{AsyncRead, AsyncWrite, Future, Stream};
 
 use crate::config::parse_nts_ke_config;
 
+use crate::cookie;
 use crate::cookie::NTSKeys;
 
 struct NtsKeRecord {
@@ -27,10 +27,10 @@ struct NtsKeRecord {
     contents: Vec<u8>,
 }
 
-fn serialize_record(rec: NtsKeRecord) -> Vec<u8> {
+fn serialize_record(rec: &mut NtsKeRecord) -> Vec<u8> {
     let mut out: Vec<u8> = Vec::new();
-    let our_type = 0;
-    if critical {
+    let mut our_type = 0;
+    if rec.critical {
         our_type = 1 << 15 + rec.record_type;
     } else {
         our_type = rec.record_type;
@@ -38,7 +38,7 @@ fn serialize_record(rec: NtsKeRecord) -> Vec<u8> {
     out.extend(our_type.to_be_bytes().iter().clone());
     let our_len = rec.contents.len() as u16;
     out.extend(our_len.to_be_bytes().iter().clone());
-    out.append(contents);
+    out.append(&mut rec.contents);
     return out;
 }
 
@@ -66,17 +66,40 @@ fn gen_key(session: &rustls::ServerSession) -> Result<NTSKeys, TLSError> {
     Ok(keys)
 }
 
-fn response(keys: NTSKeys, master_key: Vec<u8>) -> Vec<u8> {
-    let cookie = cookie::make_cookie(keys, master_key);
+fn response(keys: NTSKeys, master_key: Arc<RwLock<Vec<u8>>>) -> Vec<u8> {
+    let actual_key = master_key.read().unwrap();
+    let cookie = cookie::make_cookie(keys, &actual_key);
     let mut response: Vec<u8> = Vec::new();
+    let mut aead_rec = NtsKeRecord {
+        critical: false,
+        record_type: 4,
+        contents: vec![0, 15],
+    };
+    let mut end_rec = NtsKeRecord {
+        critical: true,
+        record_type: 0,
+        contents: vec![],
+    };
+    let mut cookie_rec = NtsKeRecord {
+        critical: false,
+        record_type: 5,
+        contents: cookie,
+    };
+
+    response.append(&mut serialize_record(&mut aead_rec));
+    response.append(&mut serialize_record(&mut cookie_rec));
+    response.append(&mut serialize_record(&mut end_rec));
+    response
 }
+
 pub fn start_nts_ke_server(config_filename: &str) {
     // First parse config for TLS server using local config module.
-    let mut parsed_config = parse_nts_ke_config(config_filename);
+    let parsed_config = parse_nts_ke_config(config_filename);
     let master_key = parsed_config.cookie_key;
+    let real_key = Arc::new(RwLock::new(master_key));
     let mut server_config = ServerConfig::new(NoClientAuth::new());
     server_config
-        .set_single_cert(parsed_config.tls_certs, parsed_config.tls_keys.remove(0))
+        .set_single_cert(parsed_config.tls_certs, parsed_config.tls_keys[0].clone())
         .expect("invalid key or certificate");
     let config = TlsAcceptor::from(Arc::new(server_config));
 
@@ -92,6 +115,7 @@ pub fn start_nts_ke_server(config_filename: &str) {
     // Now, actually setup TLS server behavior.
     let done = socket.incoming().for_each(move |conn| {
         let addr = conn.peer_addr().ok();
+        let real_key = real_key.clone();
         let done = config
             .accept(conn)
             .and_then(|socket| {
@@ -99,7 +123,7 @@ pub fn start_nts_ke_server(config_filename: &str) {
                 io::read_to_end(socket, buf)
             })
             .map(|(socket, buf)| gen_key_from_channel(socket))
-            .and_then(|(socket, key)| io::write_all(socket, response(master_key, key)))
+            .and_then(|(socket, key)| io::write_all(socket, response(key, real_key)))
             .and_then(|(stream, _)| io::flush(stream))
             .map(move |_| println!("Accept: {:?}", addr))
             .map_err(move |err| println!("Error: {:?}-{:?}", err, addr));
